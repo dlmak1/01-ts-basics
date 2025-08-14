e62bcd35da0f9575906b082c31f09b9d800f4b76
Dlmak <dlmakseverin@gmail.com>
2025-08-14 16:20:38 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/01-ts-basics/', 
+  base: '/01-ts-basics/',
   build: {
     outDir: 'dist',
+    rollupOptions: {
+      output: {
+        entryFileNames: '[name].js',
+        chunkFileNames: '[name].js',
+        assetFileNames: '[name].[ext]',
+      },
+    },
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'], // Додаємо підтримку .ts файлів
   },
 });
